21f9cdb36a7dc061724b55ecf47a7d2235bda5f0
Iganza-roy <iganzaroy@gmail.com>
2023-12-30 13:44:32 +0300

    strings

--- print3.py
+++ print3.py
@@ -4,11 +4,20 @@ print(message[0:3])
 
 print(message.upper())
 
-print(message.count('m'))
+print(message.count('name'))
 
 m2 = "major "
 m3 = "man"
+m6 = 7
+m7 = 6
 
 new = m2 + m3
 
-print (new)
+print (new, m7, m6)
+
+m4 = "good"
+m5 = "morning"
+
+new = '{} {}'.format(m4, m5)
+
+print(new)
